18f50cf80716c5f083fe9ef1543e61905d3d793f
larybino <binolaryssa@gmail.com>
2025-06-23 21:49:14 -0300

    feat: adiciona tratamento de exceção 400

--- backend/src/main/java/com/github/larybino/leilao/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/github/larybino/leilao/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.github.larybino.leilao.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -20,6 +24,24 @@ public class GlobalExceptionHandler {
         null
         );
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validException(MethodArgumentNotValidException ex, WebRequest request){
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
+        "Validation Error",
+        "Invalid input data",
+        request.getDescription(false),
+        errors
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
         
     }
 
