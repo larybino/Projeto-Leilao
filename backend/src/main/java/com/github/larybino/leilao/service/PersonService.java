70ef679f2c359b28a3240f36640dd4c6286808b6
larybino <binolaryssa@gmail.com>
2025-08-16 11:28:21 -0300

    fix: criptografia na senha

--- backend/src/main/java/com/github/larybino/leilao/service/PersonService.java
+++ backend/src/main/java/com/github/larybino/leilao/service/PersonService.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.context.Context;
 
 import com.github.larybino.leilao.exception.NotFoundException;
@@ -106,13 +107,14 @@ public class PersonService implements UserDetailsService{
             throw new IllegalArgumentException("Código expirado");
         }
 
-        person.setPassword(newPassword);
+        person.setPassword(passwordEncoder.encode(newPassword));
         person.setRecoveryCode(null);
         person.setRecoveryCodeExpiration(null);
 
         personRepository.save(person);
     }
 
+    @Transactional
     public void changePassword(String email, String oldPassword, String newPassword) {
         Person person = personRepository.findByEmail(email)
             .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado."));
